b8c4995beac9aabeea3d89a91c62711308766ec8
cfrench229 <37561748+cfrench229@users.noreply.github.com>
2018-06-27 20:17:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,85 +14,84 @@ app.config(function($routeProvider) {
 //Controller that deals with adding data to the database
 app.controller('addCtrl', function($scope, $http) {
 //function to eliminate lifespan dropdown numbers
-$scope.arr = [];
-$scope.mySwitch = true;
-$scope.checkNum = function(lifespanStart) {
+	$scope.arr = [];
+	$scope.mySwitch = true;
+	$scope.checkNum = function(lifespanStart) {
 	$scope.mySwitch=false;
 	$scope.arr=[];
-	for(var i = ++lifespanStart; i <= 20; i++) {
-		$scope.arr.push(i);
+		for(var i = ++lifespanStart; i <= 20; i++) {
+			$scope.arr.push(i);
+		}	
 	}
-}
 
 	//Function is called when a user clicks the submit button
 	$scope.myFunc = function () {
 	//if we want to add to the database, then proceed
-	if (confirm('Are you sure you want to save this breed into the database?')) {
-    var jsondata = {breed: $scope.breed,description: $scope.description,size: $scope.size,
+		if (confirm('Are you sure you want to save this breed into the database?')) {
+    		var jsondata = {breed: $scope.breed,description: $scope.description,size: $scope.size,
    					lifespan: $scope.lifespanStart+"-"+$scope.lifespanEnd+" Years"};
-    	$http({
-    		async: true,
-    		crossDomain: true,
-    		url: "https://awatoproject-1698.restdb.io/rest/dogs",
-   			method: "POST",
-    		headers: {
-    			"content-type": "application/json",
-    			"x-apikey": "5b294cd346624c7b24444fbd",
-    			"cache-control": "no-cache"
-  				  },
-  			processData: false,
-  			data: JSON.stringify(jsondata)
-			})
-       		.then(function mySuccess(response) {
-       			window.location.reload();
-    		}, function myError(response) {  
-		});
-			
-	} else {
-    //otherwise cancel the add 
-	}
-	window.location.href = "https://cfrench229.github.io/awato/#!/table";	
+    		$http({
+    			async: true,
+    			crossDomain: true,
+    			url: "https://awatoproject-1698.restdb.io/rest/dogs",
+   				method: "POST",
+    			headers: {
+    				"content-type": "application/json",
+    				"x-apikey": "5b294cd346624c7b24444fbd",
+    				"cache-control": "no-cache"
+  				  		},
+  				processData: false,
+  				data: JSON.stringify(jsondata)
+				})
+       			.then(function mySuccess(response) {
+       				window.location.reload();
+    			}, function myError(response) {  
+			});	
+		} else {
+    		//otherwise cancel the add 
+		}
+			window.location.href = "https://cfrench229.github.io/awato/#!/table";	
    	} 	
 });
 
+//Controller to deal with the table
 app.controller('tableCtrl', function($scope) {
+//searches the table for breeds based on inputs
+	$scope.search = function() {
+ 		 // Declare variables 
+	var input, filter, table, tr, td, i;
+  	input = document.getElementById("myInput");
+  	filter = input.value.toUpperCase();
+  	table = document.getElementById("dogs");
+  	tr = table.getElementsByTagName("tr");
 
-$scope.search = function() {
-  // Declare variables 
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("dogs");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[2];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-};
-
-$scope.breedName = null;
-$scope.breedSize = null;
-$scope.breedLife = null;
-$scope.breedDescript = null;
-$scope.select = function(index) {
+  		// Loop through all table rows, and hide those who don't match the search query
+  	for (i = 0; i < tr.length; i++) {
+   		td = tr[i].getElementsByTagName("td")[2];
+    		if (td) {
+      			if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        			tr[i].style.display = "";
+      			} else {
+        			tr[i].style.display = "none";
+      			}
+    		} 
+  		}
+	};
+//show data about the selected dog breed
+	$scope.breedName = null;
+	$scope.breedSize = null;
+	$scope.breedLife = null;
+	$scope.breedDescript = null;
+	$scope.select = function(index) {
 		var tabl = document.getElementById('dogs');
 		$scope.breedName =  String(tabl.rows[index+1].cells[2].innerHTML);
 		$scope.breedSize =  String(tabl.rows[index+1].cells[3].innerHTML);
 		$scope.breedLife =  String(tabl.rows[index+1].cells[4].innerHTML);
 		$scope.breedDescript =  String(tabl.rows[index+1].cells[5].innerHTML);    	
-		};
-
+	};
 });
 
-//GET IS FUNCTIONAL
+//Controller that gets information from database
 app.controller('getCtrl', function($scope, $http){
 //call add in a function on page load
      $http({
@@ -109,34 +108,31 @@ app.controller('getCtrl', function($scope, $http){
         $scope.success = response.data;
     }, function myError(response) {  
     }); 
-    
-//DEETE IS FUNCTIONAL
-
-
+//deletes selected breed from the database
 	$scope.Delete = function(index) {
-	
-	if (confirm('Are you sure you want to delete this breed from the database?')) {
-    	var table = document.getElementById('dogs');
-    	var id = table.rows[index+1].cells[0].innerHTML;
-    	var temp = String(id);
-    	var str = temp.trim();
-    	var t = String(str);
-		$http({
-       		async: true,
- 			crossDomain: true,
- 			url: "https://awatoproject-1698.restdb.io/rest/dogs/"+t,
- 			method: "DELETE",
- 			headers: {
-   			 "content-type": "application/json",
-   			 "x-apikey": "5b294cd346624c7b24444fbd",
-   			 "cache-control": "no-cache"
- 				}
-    		}).then(function mySuccess(response) {
-    		window.location.reload();
-    		}, function myError(response) {  
-    	});	
+	//if they select yes, delete it
+		if (confirm('Are you sure you want to delete this breed from the database?')) {
+    		var table = document.getElementById('dogs');
+    		var id = table.rows[index+1].cells[0].innerHTML;
+    		var temp = String(id);
+    		var str = temp.trim();
+    		var t = String(str);
+				$http({
+       				async: true,
+ 					crossDomain: true,
+ 					url: "https://awatoproject-1698.restdb.io/rest/dogs/"+t,
+ 					method: "DELETE",
+ 					headers: {
+   			 		"content-type": "application/json",
+   			 		"x-apikey": "5b294cd346624c7b24444fbd",
+   			 		"cache-control": "no-cache"
+ 					}
+    			}).then(function mySuccess(response) {
+    				window.location.reload();
+    			}, function myError(response) {  
+    		});	
 		} else {
-    // Do nothing!
-	}			
+    		//otherwise do nothing and stay on the page
+		}			
 	};
 });  
